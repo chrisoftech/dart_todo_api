167ae5587a4ca02cdb9574a55fbd48bb5ad3e2e3
christian <chrisoftech@gmail.com>
2020-06-04 18:29:53 +0000

    Installed string-validator package to validate usernames and passowrd lenght.

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,5 +1,6 @@
 import 'package:aqueduct/managed_auth.dart';
 import 'package:aqueduct_todo/aqueduct_todo.dart';
+import 'package:string_validator/string_validator.dart';
 import 'package:uuid_type/uuid_type.dart';
 
 class User extends ManagedObject<_User>
@@ -11,6 +12,22 @@ class User extends ManagedObject<_User>
 
   @Serialize(input: true, output: false)
   String password;
+
+  @override
+  ValidationContext validate({Validating forEvent = Validating.insert}) {
+    final context = super.validate(forEvent: forEvent);
+
+    if (!isEmail(username)) {
+      context.addError(
+          'Username is not valid. Enter a valid email address for username');
+    }
+
+    if (!isLength(password, 6)) {
+      context.addError('Password should have a minimum of 6 characters');
+    }
+
+    return context;
+  }
 }
 
 class _User extends ResourceOwnerTableDefinition {
